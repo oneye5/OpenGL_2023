572b798ae8d0b4d2140e334d086b47d126d49eff
oneye5 <ocjlaz@gmail.com>
2023-11-18 11:31:04 +1300

    added vertex array buffer

--- GL_2023/Renderer.cpp
+++ GL_2023/Renderer.cpp
@@ -8,6 +8,7 @@ GLFWwindow* window;
 unsigned int vertexBuffer;
 unsigned int indexBuffer;
 unsigned int shader;
+unsigned int vertexArrayObject;
 
 int Renderer::Initialize()
 {
@@ -17,6 +18,10 @@ int Renderer::Initialize()
         std::cout << "failed to initialize glfw";
         return -1;
     }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 
 
@@ -62,24 +67,22 @@ int Renderer::Initialize()
         2,3,0
     };
 
+    glGenVertexArrays(1, &vertexArrayObject);
+    glBindVertexArray(vertexArrayObject);
+
     //vertex buffer
     glGenBuffers(1, &vertexBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
     glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), verticies, GL_STATIC_DRAW);//buffer size
 
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2.0f, 0);
     glEnableVertexAttribArray(0);
-
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2.0, 0);
 
     //index buffer
     glGenBuffers(1, &indexBuffer);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indicies, GL_STATIC_DRAW);//buffer size
 
-
-    
-
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
 
     
     //create shaders
@@ -89,13 +92,10 @@ int Renderer::Initialize()
     std::string vertexShader = FileLoader::GetFileContents(path + "defaultVertex.glsl");
 
     shader = Renderer::createShader(vertexShader, fragmentShader);
-    glUseProgram(shader);
 
-    int location = glGetUniformLocation(shader, "u_Color");
-    if (location == -1)
-        std::cout << "uniform not found";
-    else
-        glUniform4f(location, 0.2f, 0.3f, 0.5f, 1.0f);
+
+    
+
 
     return 0;
 }
@@ -106,6 +106,18 @@ void Renderer::Destroy()
 }
 int Renderer::Render() //returns -1 to quit window
 {
+    //opengl stuff
+    glUseProgram(shader);
+    int location = glGetUniformLocation(shader, "u_Color");
+    if (location == -1)
+        std::cout << "uniform not found";
+    else
+        glUniform4f(location, 0.2f, 0.3f, 0.5f, 1.0f);
+
+    glBindVertexArray(vertexArrayObject);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+
     glClear(GL_COLOR_BUFFER_BIT);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,nullptr);
 
